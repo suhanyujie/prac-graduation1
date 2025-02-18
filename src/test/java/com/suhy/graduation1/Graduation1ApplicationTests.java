db80f4aaad09308d3f80b420466263a5ee64a13a
suhanyujie <suhanyujie@qq.com>
2025-02-18 16:16:29 +0900

    remove test code

--- src/test/java/com/suhy/graduation1/Graduation1ApplicationTests.java
+++ src/test/java/com/suhy/graduation1/Graduation1ApplicationTests.java
@@ -1,13 +1,9 @@
 package com.suhy.graduation1;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Graduation1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
